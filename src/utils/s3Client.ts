b81d652c51102d3a7c25c7dfc20798d6aae2e936
Matthew Thorning <matthew@ometria.com>
2022-02-05 20:24:36 +0000

    fix env issues

--- src/utils/s3Client.ts
+++ src/utils/s3Client.ts
@@ -1,9 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { config } from 'dotenv';
-config();
 
 const s3Client = new S3Client({
-  region:  'us-east-1',
+  region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
