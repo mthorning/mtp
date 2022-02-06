3f9ed7ea9de2cbf2896ecb1c55d3d0cd26691a7e
Matthew Thorning <matthew@ometria.com>
2022-02-06 18:52:40 +0000

    another simple test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,26 @@
+<script context="module" lang="ts">
+  export async function load({ params, fetch, session, stuff }) {
+    const res = await fetch('images');
+
+    const data = await res.json();
+    if (!res.ok) {
+      return {
+        status: res.status,
+        error: new Error(res.statusText)
+      };
+    }
+
+    return {
+      props: {
+        images: data
+      }
+    };
+  }
+</script>
+
+<script>
+  export let images;
+  $: console.log(images);
+</script>
+
 <p>hello</p>
